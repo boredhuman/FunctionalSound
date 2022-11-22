708e1d035e1a5d264fe705e97aa97a130ba43c1a
Daniel <55595431+boredhuman@users.noreply.github.com>
2022-11-22 17:12:15 +0000

    added constants

--- web/elements/expression_node.dart
+++ web/elements/expression_node.dart
@@ -5,6 +5,38 @@ import 'delete.dart';
 import '../main.dart';
 
 class ExpressionNode {
+  
+  static DivElement createAddRow(List<String> svgClasses, String text) {
+    return DivElement()
+      ..classes.addAll(["row", "input-row"])
+      ..style.setProperty("justify-content", "center")
+      ..children.addAll([
+      SvgSvgElement()
+        ..classes.addAll(svgClasses)
+        ..style.setProperty("width", "20px")
+        ..style.setProperty("height", "20px")
+        ..children.addAll([
+          RectElement()
+            ..setAttribute("x", "7.5")
+            ..setAttribute("y", "0")
+            ..setAttribute("width", "5")
+            ..setAttribute("height", "20")
+            ..setAttribute("rx", "2")
+            ..style.setProperty("fill", "white"),
+          RectElement()
+            ..setAttribute("x", "0")
+            ..setAttribute("y", "7.5")
+            ..setAttribute("width", "20")
+            ..setAttribute("height", "5")
+            ..setAttribute("rx", "2")
+            ..style.setProperty("fill", "white")
+        ]),
+      DivElement()..style.setProperty("min-width", "10px"),
+      ParagraphElement()
+        ..text = text
+        ..classes.add("text")
+    ]);
+  }
 
   static DivElement create(int left, int top) {
     int nodeID = id++;
@@ -26,6 +58,11 @@ class ExpressionNode {
                   ..classes.addAll(["row", "input-row"])
                   ..children.addAll([
                     DivElement()
+                      ..classes.addAll(["material-symbols-outlined"])
+                      ..style.setProperty("color", "white")
+                      ..style.setProperty("margin-left", "10px")
+                      ..text = "settings",
+                    DivElement()
                       ..classes.addAll(["row", "input-row"])
                       ..style.setProperty("justify-content", "center")
                       ..style.setProperty("flex", "1")
@@ -42,35 +79,8 @@ class ExpressionNode {
                     ..placeholder = "expression"
                     ..classes.addAll(["text", "expression-text"])
                     ..style.setProperty("text-align", "center")),
-                DivElement()
-                  ..classes.addAll(["row", "input-row"])
-                  ..style.setProperty("justify-content", "center")
-                  ..children.addAll([
-                    SvgSvgElement()
-                      ..classes.addAll(["addInput"])
-                      ..style.setProperty("width", "20px")
-                      ..style.setProperty("height", "20px")
-                      ..children.addAll([
-                        RectElement()
-                          ..setAttribute("x", "7.5")
-                          ..setAttribute("y", "0")
-                          ..setAttribute("width", "5")
-                          ..setAttribute("height", "20")
-                          ..setAttribute("rx", "2")
-                          ..style.setProperty("fill", "white"),
-                        RectElement()
-                          ..setAttribute("x", "0")
-                          ..setAttribute("y", "7.5")
-                          ..setAttribute("width", "20")
-                          ..setAttribute("height", "5")
-                          ..setAttribute("rx", "2")
-                          ..style.setProperty("fill", "white")
-                      ]),
-                    DivElement()..style.setProperty("min-width", "10px"),
-                    ParagraphElement()
-                      ..text = " Add Input"
-                      ..classes.add("text")
-                  ])
+                ExpressionNode.createAddRow(["addInput"], "Add Input"),
+                ExpressionNode.createAddRow(["addConst"], "Add Const")
               ]),
             DivElement()..classes.addAll(["joint", "output-joint"])
           ])
--- web/main.dart
+++ web/main.dart
@@ -1,4 +1,5 @@
 import 'dart:html';
+import 'dart:math';
 import 'dart:svg';
 import 'elements/delete.dart';
 import 'elements/expression_node.dart';
@@ -12,30 +13,70 @@ import 'util/util.dart';
 var id = 0;
 LineRenderer lineRenderer = LineRenderer();
 AudioManager audioManager = AudioManager();
+late DivElement elementContainer;
 
 void main() {
-  var timeNode = TimeNode.create(100, 400);
+  var timeNode = TimeNode.create(500, 400);
 
-  var outputNode = OutputNode.create(800, 440);
+  var outputNode = OutputNode.create(1200, 440);
 
   var settings = Settings.createSettings();
 
-  var elementContainer = DivElement()
+  elementContainer = DivElement()
     ..id = "nodeBox"
-    ..children.addAll([timeNode, outputNode, ExpressionNode.create(400, 400)]);
+    ..children.addAll([timeNode, outputNode, ExpressionNode.create(800, 400)]);
 
   document.body?.children.addAll([lineRenderer.background, elementContainer, settings]);
 
-  addDragAllFunction();
+  addDocumentFunctions();
   addFunctions();
 }
 
-addFunctions() {
-  addDragFunctions();
+void disableTextSelection() {
+  document.body!.style.setProperty("user-select", "none");
+}
+
+void enableTextSelection() {
+  document.body!.style.removeProperty("user-select");
+}
+
+// functions which operate on the document instead of the nodes
+addDocumentFunctions() {
+  addDragAllFunction();
+  addDoubleClickCreateFunction();
   addJointFunctions();
-  addInputFunctions();
+}
+
+addFunctions() {
+  List<Node> nodes = document.getElementsByClassName("node");
+  addDragFunctions(nodes);
+  var inputNodes = document.getElementsByClassName("addInput");
+  addInputFunctions(inputNodes);
+  var constRows = document.getElementsByClassName("addConst");
+  addConstFunctions(constRows);
   addPausePlayFunctions();
   addDeleteFunctions(document.getElementsByClassName("delete-icon"));
+
+  var constBoxes = document.getElementsByClassName("constBox");
+  addSliderFunctions(constBoxes);
+}
+
+addNodeFunctions(Element node) {
+  addDragFunctions([node]);
+  addInputFunctions(node.getElementsByClassName("addInput"));
+  addDeleteFunctions(node.getElementsByClassName("delete-icon"));
+  addConstFunctions(node.getElementsByClassName("addConst"));
+}
+
+void addDoubleClickCreateFunction() {
+  document.body?.onDoubleClick.listen((event) {
+    if (event is MouseEvent && (event.target == lineRenderer.background || event.target == document.body)) {
+      Element newNode = ExpressionNode.create(event.client.x.toInt() - 150, event.client.y.toInt());
+      // add dragging and the input functions
+      addNodeFunctions(newNode);
+      elementContainer.children.add(newNode);
+    }
+  });
 }
 
 void addDragAllFunction() {
@@ -45,15 +86,18 @@ void addDragAllFunction() {
 
   document.body!.onMouseDown.listen((event) {
     if (event.target == document.body || event.target == lineRenderer.background) {
-      consoleLog("dragging body");
       prevX = event.client.x.toInt();
       prevY = event.client.y.toInt();
       dragging = true;
+      // stop things from being highlighted
+      document.body!.style.setProperty("user-select", "none");
     }
   });
 
   document.body!.onMouseUp.listen((event) {
     dragging = false;
+    // all things to be highlighted again
+    document.body!.style.removeProperty("user-select");
   });
 
   document.body!.onMouseMove.listen((event) {
@@ -75,6 +119,8 @@ void addDragAllFunction() {
           node.style.setProperty("left", "${leftNumber + deltaX}px");
           node.style.setProperty("top", "${topNumber + deltaY}px");
         }
+
+        lineRenderer.update();
       }
     }
   });
@@ -83,7 +129,6 @@ void addDragAllFunction() {
 void addDeleteFunctions(List<Node> nodes) {
   for (Node node in nodes) {
     if (node is Element) {
-      consoleLog(node);
       node.onClick.listen((event) {
         String? deleteAttribute = node.getAttribute("delete");
         if (deleteAttribute != null) {
@@ -157,36 +202,45 @@ String resolveInputs(Element element, String expression) {
 
   for (Node srcRow in srcRows) {
     if (srcRow is Element) {
-      // get input joint to get input node
-      Element inputJoint = srcRow.getElementsByClassName("input-joint")[0] as Element;
+      // const box provide their own input
+      if (srcRow.classes.contains("constBox")) {
+        Element knobVal = srcRow.getElementsByClassName("knobVal")[0] as Element;
+        String index = (srcRow.getElementsByClassName("src-index")[0] as Element).getAttribute("index")!;
 
-      String? srcNodeID = inputJoint.getAttribute("src-node");
 
-      if (srcNodeID == null) {
-        continue;
-      }
-
-      if (srcNodeID == "time") {
-        // if the src is the time node we just replace it with x
-        List<Node> nodes = srcRow.getElementsByClassName("src-index");
-        if (nodes.isNotEmpty) {
-          String index = (nodes[0] as Element).getAttribute("index")!;
-          expression = expression.replaceAll("i$index", "x");
-        }
+        expression = expression.replaceAll("i$index", knobVal.text!);
       } else {
-        // get the src node
-        Element? srcNode = document.getElementById(srcNodeID);
+        // get input joint to get input node
+        Element inputJoint = srcRow.getElementsByClassName("input-joint")[0] as Element;
 
-        if (srcNode != null) {
-          // get the expression
-          List<Node> expressionElement = srcNode.getElementsByClassName("expression-text");
+        String? srcNodeID = inputJoint.getAttribute("src-node");
 
-          if (expressionElement.isNotEmpty) {
-            List<Node> nodes = srcRow.getElementsByClassName("src-index");
-            if (nodes.isNotEmpty) {
-              // replace the ix with the expression
-              String index = (nodes[0] as Element).getAttribute("index")!;
-              expression = expression.replaceAll("i$index", "(${resolveInputs(srcNode, (expressionElement[0] as InputElement).value!)})");
+        if (srcNodeID == null) {
+          continue;
+        }
+
+        if (srcNodeID == "time") {
+          // if the src is the time node we just replace it with x
+          List<Node> nodes = srcRow.getElementsByClassName("src-index");
+          if (nodes.isNotEmpty) {
+            String index = (nodes[0] as Element).getAttribute("index")!;
+            expression = expression.replaceAll("i$index", "x");
+          }
+        } else {
+          // get the src node
+          Element? srcNode = document.getElementById(srcNodeID);
+
+          if (srcNode != null) {
+            // get the expression
+            List<Node> expressionElement = srcNode.getElementsByClassName("expression-text");
+
+            if (expressionElement.isNotEmpty) {
+              List<Node> nodes = srcRow.getElementsByClassName("src-index");
+              if (nodes.isNotEmpty) {
+                // replace the ix with the expression
+                String index = (nodes[0] as Element).getAttribute("index")!;
+                expression = expression.replaceAll("i$index", "(${resolveInputs(srcNode, (expressionElement[0] as InputElement).value!)})");
+              }
             }
           }
         }
@@ -262,15 +316,13 @@ void addPausePlayFunctions() {
   });
 }
 
-// add input row to node logic
-void addInputFunctions() {
-  var nodes = document.getElementsByClassName("addInput");
-
+void addConstFunctions(List<Node> nodes) {
   for (var node in nodes) {
     if (node is Element) {
       node.addEventListener("click", (event) {
+        // svg -> row -> rows container
         var container = node.parent!.parent!;
-        var last = container.children.last;
+        var last = container.children[container.children.length - 2];
 
         Element? lastSrcRow;
 
@@ -283,7 +335,208 @@ void addInputFunctions() {
         int index = 0;
 
         if (lastSrcRow != null) {
-          String? indexAttrib = lastSrcRow.children[1].getAttribute("index");
+          String? indexAttrib = (lastSrcRow.getElementsByClassName("src-index")[0] as Element).getAttribute("index");
+          index = int.parse(indexAttrib!) + 1;
+        }
+
+        SvgSvgElement deleteIcon = Delete.getDeleteButton("");
+
+        addDeleteFunctions([deleteIcon]);
+
+        var newSrcRow = DivElement()
+          ..classes.addAll(["input-row", "src-row", "constBox"])
+          ..style.setProperty("margin", "5px 0")
+          ..style.setProperty("height", "120px")
+          ..children.addAll([
+            DivElement()
+              ..style.setProperty("width", "34px")
+              ..style.setProperty("transform", "translate(-50%, 0)"),
+            DivElement()
+              ..style.setProperty("height", "120px")
+              ..style.setProperty("display", "flex")
+              ..style.setProperty("align-items", "center")
+              ..style.setProperty("padding", "0 5px")
+              ..style.setProperty("margin", "0 2px")
+              ..style.setProperty("background-color", "#444")
+              ..style.setProperty("border-radius", "5px")
+              ..children.addAll([
+                ParagraphElement()
+                  ..text = "i$index :\xa0"
+                  ..classes.addAll(["text", "src-index"])
+                  ..style.setProperty("white-space", "nowrap")
+                  ..setAttribute("index", "$index"),
+                DivElement()
+                  ..style.setProperty("display", "flex")
+                  ..children.addAll([
+                    // the knob
+                    DivElement()
+                      ..style.setProperty("width", "120px")
+                      ..style.setProperty("height", "120px")
+                      ..style.setProperty("min-width", "120px")
+                      ..children.addAll([
+                        DivElement()
+                          ..style.setProperty("width", "80px")
+                          ..style.setProperty("height", "80px")
+                          ..style.setProperty("margin", "auto")
+                          ..children.addAll([
+                            SvgSvgElement()
+                              ..classes.addAll(["knob"])
+                              ..style.setProperty("width", "80px")
+                              ..style.setProperty("height", "80px")
+                              ..children.addAll([
+                                CircleElement()
+                                  ..setAttribute("cx", "40")
+                                  ..setAttribute("cy", "40")
+                                  ..setAttribute("r", "28")
+                                  ..setAttribute("fill", "#222"),
+                                CircleElement()
+                                  ..classes.addAll(["dial"])
+                                  ..setAttribute("cx", "55")
+                                  ..setAttribute("cy", "55")
+                                  ..setAttribute("r", "2")
+                                  ..setAttribute("fill", "aqua")
+                              ])
+                          ]),
+                        ParagraphElement()
+                          ..text = "1.0"
+                          ..style.setProperty("margin", "0")
+                          ..style.setProperty("text-align", "center")
+                          ..classes.addAll(["text", "knobVal"])
+                      ]),
+                  ]),
+                // the settings for it
+                DivElement()
+                  ..style.setProperty("height", "100%")
+                  ..style.setProperty("padding", "0px 5px")
+                  ..children.addAll([
+                    ParagraphElement()
+                      ..text = "Min"
+                      ..classes.addAll(["text"])
+                      ..style.setProperty("font-size", "12px")
+                      ..style.setProperty("margin", "0")
+                      ..style.setProperty("min-width", "0"),
+                    InputElement()
+                      ..value = "0"
+                      ..classes.addAll(["text", "centerText", "min"])
+                      ..style.setProperty("background-color", "#222")
+                      ..style.setProperty("width", "100%")
+                      ..style.setProperty("border-radius", "2px"),
+                    ParagraphElement()
+                      ..text = "Max"
+                      ..classes.addAll(["text"])
+                      ..style.setProperty("font-size", "12px")
+                      ..style.setProperty("margin", "0"),
+                    InputElement()
+                      ..value = "1"
+                      ..classes.addAll(["text", "centerText", "max"])
+                      ..style.setProperty("background-color", "#222")
+                      ..style.setProperty("width", "100%")
+                      ..style.setProperty("border-radius", "2px"),
+                    ParagraphElement()
+                      ..text = "Precision"
+                      ..classes.addAll(["text"])
+                      ..style.setProperty("font-size", "12px")
+                      ..style.setProperty("margin", "0"),
+                    InputElement()
+                      ..value = "2"
+                      ..classes.addAll(["text", "centerText", "precision"])
+                      ..style.setProperty("background-color", "#222")
+                      ..style.setProperty("width", "100%")
+                      ..style.setProperty("border-radius", "2px")
+                  ]),
+                deleteIcon
+              ])
+          ]);
+
+        print("adding const");
+        addSliderFunctions([newSrcRow]);
+
+        container.insertBefore(newSrcRow, last);
+      });
+    }
+  }
+}
+
+// list of const boxes
+void addSliderFunctions(List<Node> nodes) {
+  for (Node node in nodes) {
+    if (node is Element) {
+      int prevY = 0;
+      bool dragging = false;
+      double knobRotation = 1;
+
+      Element knob = node.getElementsByClassName("knob")[0] as Element;
+      InputElement minElement = node.getElementsByClassName("min")[0] as InputElement;
+      InputElement maxElement = node.getElementsByClassName("max")[0] as InputElement;
+      InputElement precisionElement = node.getElementsByClassName("precision")[0] as InputElement;
+      Element knobVal = node.getElementsByClassName("knobVal")[0] as Element;
+      Element dial = node.getElementsByClassName("dial")[0] as Element;
+
+      knob.onMouseDown.listen((event) {
+        dragging = true;
+        prevY = event.client.y.toInt();
+        disableTextSelection();
+      });
+
+      document.onMouseUp.listen((event) {
+        dragging = false;
+        enableTextSelection();
+      });
+
+      document.onMouseMove.listen((event) {
+        if (dragging) {
+          int currentY = event.client.y.toInt();
+          // if vertical position changed update slider
+          if (currentY != prevY) {
+            String? minText = minElement.value;
+            double minVal = minText != null ? double.parse(minText) : 0;
+            String? maxText = maxElement.value;
+            double maxVal = maxText != null ? double.parse(maxText) : 0;
+            String? precisionText = precisionElement.value;
+            int precision = precisionText != null ? int.parse(precisionText) : 1;
+
+            double delta = (currentY - prevY) / 100;
+            knobRotation = (knobRotation += delta).clamp(0, 1);
+            var angle = 90 + (270 * knobRotation);
+
+            double unrounded = (minVal + (maxVal - minVal) * knobRotation).clamp(minVal, maxVal);
+            knobVal.text = unrounded.clamp(minVal, maxVal).toStringAsFixed(precision);
+
+            var angleRad = angle * pi / 180;
+            var newX = 15 * cos(angleRad) - 15 * sin(angleRad);
+            var newY = 15 * sin(angleRad) + 15 * cos(angleRad);
+            dial
+              ..setAttribute("cx", (40 + newX).toInt().toString())
+              ..setAttribute("cy", (40 + newY).toInt().toString());
+
+            prevY = currentY;
+          }
+        }
+      });
+    }
+  }
+}
+
+// add input row to node logic
+void addInputFunctions(List<Node> nodes) {
+  for (var node in nodes) {
+    if (node is Element) {
+      node.addEventListener("click", (event) {
+        var container = node.parent!.parent!;
+        var last = container.children[container.children.length - 2];
+
+        Element? lastSrcRow;
+
+        for (Element element in container.children) {
+          if (element.classes.contains("src-row")) {
+            lastSrcRow = element;
+          }
+        }
+
+        int index = 0;
+
+        if (lastSrcRow != null) {
+          String? indexAttrib = (lastSrcRow.getElementsByClassName("src-index")[0] as Element).getAttribute("index");
           index = int.parse(indexAttrib!) + 1;
         }
 
@@ -293,21 +546,33 @@ void addInputFunctions() {
 
         var newSrcRow = DivElement()
           ..classes.addAll(["row", "input-row", "src-row"])
+          ..style.setProperty("margin", "5px 0")
           ..children.addAll([
             DivElement()
               ..classes.addAll(["joint", "input-joint"])
               ..style.setProperty("transform", "translate(-50%, 0)"),
-            ParagraphElement()
-              ..text = "i$index :\xa0"
-              ..classes.addAll(["text", "src-index"])
-              ..style.setProperty("white-space", "nowrap")
-              ..setAttribute("index", "$index"),
-            InputElement(type: "text")
-              ..placeholder = "untitled module"
-              ..classes.add("text")
-              ..style.setProperty("text-align", "left")
-              ..style.setProperty("min-width", "0"),
-            deleteIcon
+            DivElement()
+              ..style.setProperty("height", "40px")
+              ..style.setProperty("display", "flex")
+              ..style.setProperty("background-color", "#444")
+              ..style.setProperty("margin", "0 2px")
+              ..style.setProperty("padding", "0 5px")
+              ..style.setProperty("align-items", "center")
+              ..style.setProperty("border-radius", "5px")
+              ..children.addAll([
+                ParagraphElement()
+                  ..text = "i$index :\xa0"
+                  ..classes.addAll(["text", "src-index"])
+                  ..style.setProperty("white-space", "nowrap")
+                  ..setAttribute("index", "$index"),
+                InputElement(type: "text")
+                  ..placeholder = "untitled input"
+                  ..classes.add("text")
+                  ..style.setProperty("text-align", "left")
+                  ..style.setProperty("width", "100%")
+                  ..style.setProperty("background-color", "#00000000"),
+                deleteIcon
+              ])
           ]);
 
         container.insertBefore(newSrcRow, last);
@@ -349,9 +614,7 @@ void addJointFunctions() {
   });
 }
 
-void addDragFunctions() {
-  List<Node> nodes = document.getElementsByClassName("node");
-
+void addDragFunctions(List<Node> nodes) {
   for (var node in nodes) {
     if (node is Element) {
       for (var child in node.children) {
@@ -375,14 +638,14 @@ void addDragFunctions() {
             child.style.setProperty("background-color", "red");
 
             // stop text from thinking its being selected
-            document.body!.style.setProperty("user-select", "none");
+            disableTextSelection();
           });
 
           document.onMouseUp.listen((event) {
             dragging = false;
             child.style.setProperty("background-color", "white");
             // allow text to be highlighted again
-            document.body!.style.removeProperty("user-select");
+            enableTextSelection();
           });
 
           document.onMouseMove.listen((event) {
